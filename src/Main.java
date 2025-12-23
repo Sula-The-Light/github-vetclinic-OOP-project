ca4936b47aef22249bb11a8d7df9d4941f0886b4
Sultan <esmagzam66@gmail.com>
2025-12-23 10:28:22 +0500

    init commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Pet pet1 = new Pet("Ross", 3, "dog", "male", "temperature");
         Owner owner1 = new Owner(1456453, "Temirlan", "777777", "EXPO");
-        Veterenarian vet = new Veterenarian("Birzhan Rukhanov", "666666", 10);
+        Veterenarian vet = new Veterenarian("Birzhan Rukhanov", "666666", 15);
         Treatment treatment = new Treatment("Paracetamol", 3000, 2028);
         Vaccination vaccination = new Vaccination("Calicivirus", "Rabies" , 2025 ,2027);
         Appointment appointment = new Appointment(pet1, vet, true);
